1a9bb0317cf6c4b1e62c83e4268d0b5c66bec4f3
JakeShino <jshinoha@stevens.edu>
2024-12-13 03:56:54 -0700

    Update index.js

--- data/index.js
+++ data/index.js
@@ -1,6 +1,8 @@
 // This file should import both data files and export them as shown in the lecture code
 import * as recipeDataFunctions from './recipes.js';
 import * as userDataFunctions from './users.js';
+import * as shoppingListDataFunctions from './shoppingList.js';
 
 export const recipeData = recipeDataFunctions;
 export const userData = userDataFunctions;
+export const shoppingListData = shoppingListDataFunctions;
